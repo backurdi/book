a61775cf91b8fdce6741af1b31e078c0d629576b
backurdi <bachir.kurdi@gmail.com>
2022-02-09 07:55:21 +0100

    Update other.js

--- src/store/modules/other.js
+++ src/store/modules/other.js
@@ -47,18 +47,41 @@ const otherStore = {
       });
     },
     async subscribeForNotifications() {
+      let subscription;
       navigator.serviceWorker.getRegistrations().then(async () => {
         // if (!registrations.length) {
         // Register Service Worker
-        const register = await navigator.serviceWorker.register("/worker.js", {
-          scope: "/",
-        });
+        navigator.serviceWorker
+          .register("/worker.js", {
+            scope: "/",
+          })
+          .then(async function (registration) {
+            var serviceWorker;
+            if (registration.installing) {
+              serviceWorker = registration.installing;
+              console.log("installing");
+            } else if (registration.waiting) {
+              serviceWorker = registration.waiting;
+              console.log("waiting");
+            } else if (registration.active) {
+              serviceWorker = registration.active;
 
-        // Register Push
-        const subscription = await register.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey: urlBase64ToUint8Array(import.meta.env.VITE_PUBLIC_VAPI_KEY),
-        });
+              // Register Push
+              subscription = await registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(import.meta.env.VITE_PUBLIC_VAPI_KEY),
+              });
+              console.log("active");
+            }
+            if (serviceWorker) {
+              // logState(serviceWorker.state);
+              serviceWorker.addEventListener("statechange", function (e) {
+                console.log(e);
+                // logState(e.target.state);
+              });
+            }
+          })
+          .catch((err) => console.log(err));
 
         // Send Push Notification
         await this.$api.notification.post(JSON.stringify(subscription));
